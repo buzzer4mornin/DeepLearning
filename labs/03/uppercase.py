0d02a6d0c8c31183eb3664d00a89df0b631cc86e
ahmadliaydin <aydin.ahmadli@merck.com>
2021-03-29 01:55:35 +0400

    update

--- labs/03/uppercase.py
+++ labs/03/uppercase.py
@@ -17,20 +17,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--alphabet_size", default=50, type=int,
                     help="If nonzero, limit alphabet to this many most frequent chars.")
 parser.add_argument("--batch_size", default=1000, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=500, type=int, help="Number of epochs.")
+parser.add_argument("--epochs", default=1, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
 parser.add_argument("--window", default=4, type=int, help="Window size to use.")
 # my additional args
-parser.add_argument("--dropout", default=0.3, type=float, help="Dropout regularization.")
-parser.add_argument("--l2", default=0.001, type=float, help="L2 regularization.")
+parser.add_argument("--dropout", default=0.1, type=float, help="Dropout regularization.")
+parser.add_argument("--l2", default=0.0001, type=float, help="L2 regularization.")
 parser.add_argument("--hidden_layers", default=[100, 100, 100], nargs="*", type=int, help="Hidden layer sizes.")
 parser.add_argument("--decay", default="polynomial", type=str, help="polynomial | exponential | Learning decay rate type")
 parser.add_argument("--learning_rate", default=0.001, type=float, help="Initial learning rate.")
-parser.add_argument("--learning_rate_final", default=0.0001, type=float, help="Final learning rate.")
+parser.add_argument("--learning_rate_final", default=0, type=float, help="Final learning rate.")
 
 
 def main(args):
+
     # Fix random seeds and threads
     np.random.seed(args.seed)
     tf.random.set_seed(args.seed)
@@ -47,12 +48,12 @@ def main(args):
     # Load data
     # uppercase_data = UppercaseData(args.window, args.alphabet_size)
     # Save data for easy access
-    # with open("uppercase_alphabet.model", 'wb') as f:
+    # with open("uppercase_alphabet.data", 'wb') as f:
     #    pickle.dump(uppercase_data, f)
 
     # Load saved data
     # DATA PARAMETERS: train.size = 6,111,990 || test.size  = 363,932 || dev.size   = 362,988
-    with open("uppercase_alphabet.model", 'rb') as f:
+    with open("uppercase_alphabet.data", 'rb') as f:
         uppercase_data = pickle.load(f)
 
     # TODO: Implement a suitable model, optionally including regularization, select
@@ -117,17 +118,71 @@ def main(args):
         validation_data=(uppercase_data.dev.data["windows"], uppercase_data.dev.data["labels"]),
         verbose=2,
     )
+    # model.summary()
 
-    model.summary()
+    # ------------------------------------------- Test on Dev ----------------------------------------------------------
+    dev_text = uppercase_data.dev.text
+    prediction = np.round(model.predict(uppercase_data.dev.data["windows"], batch_size=args.batch_size))
+    dev_pred = ""
+    num_pred = 0
+    for character in range(len(dev_text)):
+        if dev_text[character].upper() == dev_text[character].lower():
+            dev_pred = dev_pred + dev_text[character]
+        else:
+            if prediction[num_pred] == 1:
+                char = dev_text[character].upper()
+                dev_pred = dev_pred + char[0]
+            else:
+                char = dev_text[character].lower()
+                dev_pred = dev_pred + char[0]
+            num_pred += 1
 
+    same_dev = 0
+    for character in range(len(dev_text)):
+        same_dev += dev_text[character] == dev_pred[character]
+    print("Accuracy:", 100 * same_dev / len(dev_text))
+
+    # ------------------------------------------- Test on Test ---------------------------------------------------------
+    '''test_text = uppercase_data.test.text
+    prediction = np.round(model.predict(uppercase_data.test.data["windows"], batch_size=args.batch_size))
+    test_pred = ""
+    num_pred = 0
+    for character in range(len(test_text)):
+        if test_text[character].upper() == test_text[character].lower():
+            test_pred = test_pred + test_text[character]
+        else:
+            if prediction[num_pred] == 1:
+                char = test_text[character].upper()
+                test_pred = test_pred + char[0]
+            else:
+                char = test_text[character].lower()
+                test_pred = test_pred + char[0]
+            num_pred += 1
+
+    same_test = 0
+    for character in range(len(test_text)):
+        same_test += test_text[character] == test_pred[character]
+    print("Accuracy:", 100 * same_test / len(test_text))'''
+    # -------------------------------------------- Save Model ----------------------------------------------------------
     # TODO: Generate correctly capitalized test set.
     # Use `uppercase_data.test.text` as input, capitalize suitable characters,
     # and write the result to predictions_file (which is
     # `uppercase_test.txt` in the `args.logdir` directory).
-
-    # TODO UnCOMMENT BELOW
-    # with open(os.path.join(args.logdir, "uppercase_test.txt"), "w", encoding="utf-8") as predictions_file:
-    #    ...
+    ''' model.save('model_' +
+                   str(args.hidden_layers) + '_' + str(args.alphabet_size) + '_' +
+                   str(args.window) + '_' + str(args.epochs) + '_' + str(args.l2) + '_' +
+                   str(args.learning_rate) + '_' + str(args.learning_rate_final) + '_' +
+                   str(args.decay) + '_' + str(args.dropout) + '_' + '.h5')'''
+    test_prediction = np.round(model.predict(uppercase_data.test.data["windows"], batch_size=args.batch_size))
+    with open('uppercase_test_' + str(args.hidden_layers) + '_' + str(args.alphabet_size) + '_' +
+                   str(args.window) + '_' + str(args.epochs) + '_' + str(args.l2) + '_' +
+                   str(args.learning_rate) + '_' + str(args.learning_rate_final) + '_' +
+                   str(args.decay) + '_' + str(args.dropout) + '.txt', "w", encoding="utf-8") as out_file:
+        for character in range(test_prediction.shape[0]):
+            if test_prediction[character] == 1:
+                print(uppercase_data.test.text[character].upper(), file=out_file, end='')
+            else:
+                print(uppercase_data.test.text[character].lower(), file=out_file, end='')
 
 
 if __name__ == "__main__":
